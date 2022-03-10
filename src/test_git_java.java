c5c6b2554a69c504bb53d39d3948fb53b27f494f
Asadov Zack <Khan.101.zack.1806@outlook.com>
2022-03-10 17:37:44 +0800

    first commit

---
+++ src/test_git_java.java
@@ -0,0 +1,9 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class test_git_java {
+    public static void main(String[] args) {
+        SimpleDateFormat formmatchik = new SimpleDateFormat("yyyy/MM/dd");
+        System.out.println(formmatchik.format(new Date()));
+    }
+}
