5c702e7e5603464de406ea8fe7798da63c191db2
Asadov Zack <Khan.101.zack.1806@outlook.com>
2022-03-19 18:00:27 +0800

    master branch some knowleges about git

--- src/test_git_java.java
+++ src/test_git_java.java
@@ -8,5 +8,7 @@ public class test_git_java {
         System.out.println("change something for check");
         System.out.println("it's for checking git update");
         System.out.println("it's only for conflict in git versions from 2.8");
+        System.out.println("git checkout -b branch_name creates new branch and connect to the new branch");
+        System.out.println("git branch , to check branches");
     }
 }
